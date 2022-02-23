7e315198851fea22f561269f40e13aa84d775bd2
PradHolla <pradholla@gmail.com>
2022-02-23 23:04:23 +0530

    Setup ENV Configuration

--- app/database.py
+++ app/database.py
@@ -1,9 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:cnhpnhcnh@localhost:5432/fastapiApp"
-
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+# while True:
+#     try:
+#         conn = psycopg2.connect(host="localhost", database="fastapiApp", user="postgres", password="cnhpnhcnh", cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Connected to the database!")
+#         break
+#     except Exception as e:
+#         print("Unable to connect to the database")
+#         print(e)
+#         time.sleep(3)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
