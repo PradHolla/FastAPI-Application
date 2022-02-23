f748e02088557718529c8abf11c966f33c9add0c
PradHolla <pradholla@gmail.com>
2022-02-23 23:06:06 +0530

    User Verification for Deletion and Updating

--- app/routers/post.py
+++ app/routers/post.py
@@ -2,24 +2,26 @@ from fastapi import Body, FastAPI, Response, status, HTTPException, Depends, API
 from sqlalchemy.orm import Session
 from .. import models, schemas, oauth2
 from ..database import get_db
-from typing import List
+from typing import List, Optional
 
 router = APIRouter()
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = None):
     # posts = cursor.execute("SELECT * FROM posts")
     # posts = cursor.fetchall()
     # print(posts)
-    return db.query(models.Post).all()
+    # return db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
+    
+    return db.query(models.Post).filter(models.Post.title.contains(search)).offset(skip).limit(limit).all()
+    # return db.query(models.Post).all()
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post:schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * ", (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
-    print(current_user.email)
-    new_post = models.Post(**post.dict())
+    new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -30,8 +32,12 @@ def get_post(id: int, response: Response, db: Session = Depends(get_db), current
     # cursor.execute("SELECT * FROM posts WHERE id = %s", (str(id),))
     # post = cursor.fetchone()
     post = db.query(models.Post).filter(models.Post.id == id).first()
+
     if not post:
-        raise HTTPException(status_code=404, detail=f"Post with ID: {id} not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with ID: {id} not found")
+
+    # if post.owner_id != current_user.id:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to access this post")
 
     return post
 
@@ -42,8 +48,11 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depe
     # conn.commit()
     del_post = db.query(models.Post).filter(models.Post.id == id)
     if del_post.first() is None:
-        raise HTTPException(status_code=404, detail=f"Post with ID: {id} not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with ID: {id} not found")
     
+    if del_post.first().owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not authourized to perform this action")
+
     del_post.delete(synchronize_session=False)
     db.commit()
     return Response(status_code = status.HTTP_204_NO_CONTENT)
@@ -57,6 +66,9 @@ def update_post(id: int, post: schemas.PostUpdate, db: Session = Depends(get_db)
     first_post = post_query.first()
     if first_post is None:
         raise HTTPException(status_code=404, detail=f"Post with ID: {id} not found")
+
+    if first_post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not authourized to perform this action")
 
     post_query.update(post.dict(), synchronize_session=False)
     db.commit()
