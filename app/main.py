441fb9c168c6f2c6c64b46db511bc267070a202f
PradHolla <pradholla@gmail.com>
2022-02-26 16:20:48 +0530

    Add CORS Auth

--- app/main.py
+++ app/main.py
@@ -2,10 +2,20 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
-
-models.Base.metadata.create_all(bind=engine)
+from fastapi.middleware.cors import CORSMiddleware
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
